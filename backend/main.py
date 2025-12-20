64dd13798056e7633f92917130f701a0bed41906
Benjamin Chong <ben.chong0248@gmail.com>
2025-12-19 20:28:29 -0800

    connected frontend and backend
    connected the front end the backend and the api using javascript

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from backend.agent import run_agent #must be backend.agent because of how the project is ran. it is ran from the root so when looking for imports it needs to be backend (for uvicorn)
 
+#use this to run uvicorn backend.main:app --reload
+
 app = FastAPI() #creates a fastAPI application instance
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],  # frontend origin     
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Question(BaseModel): #any request must look like this. input validation. 
     question: str 
 
-@app.post("/ask") #creates an endpoint 
+@app.post("/ask") #creates an endpoint
 def ask_question(payload: Question): #payload is automatically parsed from json and uses the question class to ensure input validation
     answer = run_agent(payload.question) #calls the entire pipeline
     return {"answer": answer}
